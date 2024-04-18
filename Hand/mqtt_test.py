534d0ef35a305d84e4ee1c04bbd530cbc4a05da1
irenkolarova <irenskolarova@gmail.com>
2024-04-18 13:38:12 +0100

    clean

--- Hand/mqtt_test.py
+++
@@ -1,8 +0,0 @@
-import paho.mqtt.publish as publish
- 
-MQTT_SERVER = "192.168.0.108"
-MQTT_PATH = "test_channel"
-import time
-while True:
-    publish.single(MQTT_PATH, "Hello World!", hostname=MQTT_SERVER) 
-    time.sleep(3)
